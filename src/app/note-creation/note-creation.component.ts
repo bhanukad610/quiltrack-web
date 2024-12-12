daeb39ba744ae7a14e3f70d8aa0fbae94cd18fea
bhanukadissanayake <bhanuka.16@cse.mrt.ac.lk>
2024-12-12 23:32:01 +0530

    Implement note creation page

--- src/app/note-creation/note-creation.component.ts
+++ src/app/note-creation/note-creation.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note-creation',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './note-creation.component.html',
   styleUrl: './note-creation.component.css'
 })
 export class NoteCreationComponent {
+  constructor(private router: Router){}
 
+  note = {title: '', content: ''};
+
+  onSubmit(){
+    if(this.note.title != '' || this.note.content != ''){
+      console.log('Note created:', this.note);
+    }
+    
+    this.router.navigate(['/']); // Navigate back to the notes list
+  }
 }
